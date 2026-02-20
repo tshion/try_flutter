85fe30fe3e3de6f04b084b43bb5e6630e1a7312d
tshion <tenta_shion@yahoo.co.jp>
2026-02-20 23:00:01 +0900

    TryKMP から取得したデータのUI 仮表示

--- lib/screens/top.screen.dart
+++ lib/screens/top.screen.dart
@@ -12,21 +12,19 @@ class TopScreen extends StatefulWidget {
 }
 
 class _TopScreenState extends State<TopScreen> {
-  int _counter = 0;
+  String _text = 'Loading...';
   final _trykmp = TryKmpHostApi();
 
-  void _incrementCounter() {
-    _trykmp.time().then((value) => print(value));
-    _trykmp.searchGitHubRepo("flutter").then((value) => print(value));
-
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _trykmp
+        .searchGitHubRepo("flutter")
+        .then(
+          (value) => setState(() {
+            _text = value;
+          }),
+        );
   }
 
   @override
@@ -41,38 +39,19 @@ class _TopScreenState extends State<TopScreen> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: Text(_text),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () => _showTimer(context),
+        tooltip: 'Show current time',
+        child: const Icon(Icons.timer),
       ),
     );
+  }
+
+  void _showTimer(BuildContext context) async {
+    final time = await _trykmp.time();
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(time)));
+    }
   }
 }
